3ceb701f50ccc02a96b7987fe49d4c52e4940ef4
Maurits van Hassel <mcvanhassel@gmail.com>
2018-04-18 22:46:37 +0200

    Update EntityMetadataCollection filter method signature

--- src/metadata-args/collection/EntityMetadataCollection.ts
+++ src/metadata-args/collection/EntityMetadataCollection.ts
@@ -13,12 +13,12 @@ export class EntityMetadataCollection extends Array<EntityMetadata> {
     hasTarget(target: Function|string) {
         return !!this.find(metadata => metadata.target === target || (typeof target === "string" && metadata.targetName === target));
     }
-    
+
     findByTarget(target: Function|string): EntityMetadata {
         const metadata = this.find(metadata => metadata.target === target || (typeof target === "string" && metadata.targetName === target));
         if (!metadata)
             throw new EntityMetadataNotFound(target);
-        
+
         return metadata;
     }
 
@@ -26,11 +26,11 @@ export class EntityMetadataCollection extends Array<EntityMetadata> {
         const metadata = this.find(metadata => metadata.name === name);
         if (!metadata)
             throw new EntityMetadataNotFound(name);
-        
+
         return metadata;
     }
 
-    filter(callbackfn: (value: EntityMetadata, index?: number, array?: Array<EntityMetadata>) => any, thisArg?: any): EntityMetadataCollection {
+    filter(callbackfn: (value: EntityMetadata, index: number, array: Array<EntityMetadata>) => any, thisArg?: any): EntityMetadataCollection {
         thisArg = thisArg || void 0;
         return this.reduce(function(out: EntityMetadataCollection, val: EntityMetadata, index: number, array: Array<EntityMetadata>) {
             if (callbackfn.call(thisArg, val, index, array)) {
@@ -39,5 +39,5 @@ export class EntityMetadataCollection extends Array<EntityMetadata> {
             return out;
         }, new EntityMetadataCollection());
     }
-    
+
 }
